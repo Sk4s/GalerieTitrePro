86923c5e48f605233c04adbe6c0cda5db8a2615a
Kévin Beauguet <k.beauguet@gmail.com>
2018-05-17 14:10:21 +0200

    Update Milolib Gallery

--- resources/views/milolib.blade.php
+++ resources/views/milolib.blade.php
@@ -20,9 +20,13 @@
 
 <div class="container gallery-container-Milolib">
 
-    <h1>Milolib (WIP)</h1>
+    <h1>Milolib</h1>
 
     <p class="page-description text-center"><a href="https://github.com/alihasana/MILOLIB" target="_blank">https://github.com/alihasana/MILOLIB</a></p>
+    <br>
+    <p class="page-description text-center"><a href="https://secret-mesa-15279.herokuapp.com/#/" target="_blank">Live Staff Part</a></p>
+    <br>
+    <p class="page-description text-center"><a href="https://secret-waters-44598.herokuapp.com/#/" target="_blank">Live Client Part</a></p>
     
     <div class="tz-gallery">
 
